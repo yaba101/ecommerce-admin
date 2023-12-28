b3e9e93be32789f43c85123f37da158838882885
Yeabsira Mekuria <yabumek46@gmail.com>
2023-12-29 01:53:32 +0300

    feat: add store popover navbar

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import prisma from "@/lib/prismadb";
+import Navbar from "@/components/navbar";
 
 export default async function DashboardLayout({
   children,
@@ -23,7 +24,7 @@ export default async function DashboardLayout({
 
   return (
     <div className="">
-      <h1>this is navbar</h1>
+      <Navbar />
       {children}
     </div>
   );
