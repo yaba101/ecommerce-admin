3db22cc8f228bf018433fb892b81bac7aceead3b
Yeabsira Mekuria <yabumek46@gmail.com>
2023-12-28 21:36:03 +0300

    feat: add global modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,23 @@
-import { UserButton } from '@clerk/nextjs'
-import Link from 'next/link'
+"use client";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 
 const SetUpPage = () => {
+  const isOpen = useStoreModal((state) => state.isOpen)
+  const onOpen = useStoreModal((state) => state.onOpen)
+  
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  },[isOpen,onOpen])
   return (
-     <div className="h-screen p-4">
-      <UserButton afterSignOutUrl='/' />
+    <div className="h-screen p-4">
+      Root Page
     </div>
-  )
-}
+  );
+};
 
-export default SetUpPage
+export default SetUpPage;
---
+++ components/Modals/store-modal.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import Modal from "@/components/ui/modal"
+import { useStoreModal } from "@/hooks/use-store-modal"
+
+const StoreModal = () => {
+    const storeModal = useStoreModal()
+    
+    return <Modal title="Create store" description="Add a new store to manage products and categories" isOpen={storeModal.isOpen} onClose={storeModal.onClose}>Future Create Form</Modal>
+}
+
+export default StoreModal
